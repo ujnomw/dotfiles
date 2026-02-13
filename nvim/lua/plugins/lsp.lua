1f4d67a966c7051b35d24d328a6649f7bd17d2dc
Mark Golovatskikh <m.golovatskih@gmail.com>
2026-02-14 00:16:15 +0100

    add gdb

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -291,6 +291,7 @@ return {
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
       'codelldb',
+      'cppdbg',
     })
     require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
 
--- nvim/lua/plugins/new-dap.lua
+++ nvim/lua/plugins/new-dap.lua
@@ -9,7 +9,9 @@ return {
     config = function()
       local dap = require('dap')
       local dapui = require('dapui')
+
       dapui.setup()
+
       dap.listeners.after.event_initialized['dapui_config'] = function()
         dapui.open()
       end
@@ -21,6 +23,7 @@ return {
       end
     end,
   },
+
   {
     'jay-babu/mason-nvim-dap.nvim',
     event = 'VeryLazy',
@@ -33,9 +36,57 @@ return {
     },
     ensure_installed = {
       'codelldb',
+      'cppdbg', -- 👈 GDB support
     },
   },
+
   {
     'mfussenegger/nvim-dap',
+    config = function()
+      local dap = require('dap')
+
+      -- GDB adapter (cppdbg)
+      dap.adapters.cppdbg = {
+        id = 'cppdbg',
+        type = 'executable',
+        command = vim.fn.stdpath('data') .. '/mason/bin/OpenDebugAD7',
+        options = {
+          detached = false,
+        },
+      }
+
+      -- GDB launch config
+      dap.configurations.cpp = {
+        {
+          name = 'Launch (gdb)',
+          type = 'cppdbg',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopAtEntry = false,
+          MIMode = 'gdb',
+          miDebuggerPath = '/usr/bin/gdb',
+          setupCommands = {
+            {
+              description = 'Enable pretty printing',
+              text = '-enable-pretty-printing',
+              ignoreFailures = true,
+            },
+          },
+        },
+        {
+          name = 'Attach (gdb)',
+          type = 'cppdbg',
+          request = 'attach',
+          processId = require('dap.utils').pick_process,
+          MIMode = 'gdb',
+          miDebuggerPath = '/usr/bin/gdb',
+        },
+      }
+
+      -- dap.configurations.c = dap.configurations.cpp
+    end,
   },
 }
